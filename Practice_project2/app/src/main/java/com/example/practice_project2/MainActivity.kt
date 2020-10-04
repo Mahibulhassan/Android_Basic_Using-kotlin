c30050087e743b20c3f1001126697a804ad23ae7
unknown <mahibulhassan842@gmail.com>
2020-10-04 22:22:39 +0600

    login page design

---
+++ Practice_project2/app/src/main/java/com/example/practice_project2/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.practice_project2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_profile_activity.*
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        this.setTitle("Login")
+
+        btn_login.setOnClickListener(this)
+        tv_register.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        if(v?.id == R.id.btn_login)
+        {
+            //Sharepreference
+
+            val sharepreferece = getSharedPreferences("userinfo",0)
+            val save_password =(sharepreferece.getString("Password",""))
+            val save_email=(sharepreferece.getString("Email",""))
+
+            // edit text details
+            val edit_email=edtext_email.text.toString()
+            val edit_password =edtext_password.text.toString()
+
+            if(save_email.equals(edit_email) && save_password.equals(edit_password)){
+                val intent = Intent(this,Profile_activity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(applicationContext,"Wrong email or password",Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        if(v?.id==R.id.tv_register)
+        {
+            val intent = Intent(this, Register_activity::class.java)
+                startActivity(intent)
+        }
+    }
+
+
+}
---
+++ Practice_project2/app/src/main/java/com/example/practice_project2/Register_activity.kt
@@ -0,0 +1,55 @@
+package com.example.practice_project2
+
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import kotlinx.android.synthetic.main.activity_register_activity.*
+
+class Register_activity : AppCompatActivity(),View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_activity)
+        this.setTitle("Register")
+
+        btn_register.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+      if(v?.id == R.id.btn_register){
+
+          val name =edit_name.text.toString()
+          val phone =edit_phone.text.toString()
+          val email =edit_email.text.toString()
+          val userid =edit_userid.text.toString()
+          val password =edit_password.text.toString()
+          val repassword =edit_repassword.text.toString()
+          val weburl =edit_webaddress.text.toString()
+
+          val sharepreferece = getSharedPreferences("userinfo",0)
+          val editor = sharepreferece.edit()
+
+          editor.putString("Name",name)
+          editor.putString("Phone",phone)
+          editor.putString("Email",email)
+          editor.putString("Userid",userid)
+          editor.putString("Password",password)
+          editor.putString("Weburl",weburl)
+
+          editor.apply()
+
+          Toast.makeText(applicationContext,"Data Register",Toast.LENGTH_LONG).show()
+
+          val intent = Intent(this,MainActivity::class.java)
+          startActivity(intent)
+      }
+    }
+
+
+
+
+}
