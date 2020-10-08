a2976fb680e43a123c8f919846237b60958a5740
Mahibulhassan <mahibulhassan842@gmail.com>
2020-10-08 15:10:49 +0600

    assignment 3

--- Practice_project2/app/src/main/java/com/example/practice_project2/MainActivity.kt
+++ Practice_project2/app/src/main/java/com/example/practice_project2/MainActivity.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_profile_activity.*
-
 class MainActivity : AppCompatActivity(),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
