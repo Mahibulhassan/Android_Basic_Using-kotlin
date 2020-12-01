2bf929548845c861ca710a7e5bffe57b46ee05a1
Mahibulhassan <mahibulhassan842@gmail.com>
2020-12-01 22:28:47 +0600

    Google push notification

---
+++ Firebase_PushNotificatin_Analytics/app/src/main/java/com/mahibul/firebase_pushnotificatin_analytics/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mahibul.firebase_pushnotificatin_analytics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
