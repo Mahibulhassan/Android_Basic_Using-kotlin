9d3fcab5f9005fd4a74a5c4aff6ccb952efd3cf0
Mahibulhassan <mahibulhassan842@gmail.com>
2021-12-11 12:49:52 +0600

    modify registration Fragment

--- DccDigitalWeek/app/src/main/java/com/mahibul/dccdigitalweek/ui/Registration/RegistrationFragment.kt
+++ DccDigitalWeek/app/src/main/java/com/mahibul/dccdigitalweek/ui/Registration/RegistrationFragment.kt
@@ -2,12 +2,11 @@ package com.mahibul.dccdigitalweek.ui.Registration
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebViewClient
+import android.widget.Toast
 import com.mahibul.dccdigitalweek.databinding.FragmentRegistrationBinding
 
 class RegistrationFragment : Fragment() {
@@ -23,7 +22,43 @@ class RegistrationFragment : Fragment() {
         _binding = FragmentRegistrationBinding.inflate(inflater,container,false)
         val root: View = binding.root
 
+        binding.btnSubmit.setOnClickListener {
+            val events = mutableListOf<String>()
+            events.clear()
+            if (binding.cheackprogramming.isChecked){
+                events.add(binding.cheackprogramming.text.toString())
+            }
+            if (binding.cheackbanner.isChecked){
+                events.add(binding.cheackbanner.text.toString())
+            }
+            if (binding.cheackwebdegin.isChecked){
+                events.add(binding.cheackwebdegin.text.toString())
+            }
+            if (binding.cheackideasub.isChecked){
+                events.add(binding.cheackideasub.text.toString())
+            }
 
+            if (binding.cheackolymp.isChecked){
+                events.add(binding.cheackolymp.text.toString())
+            }
+            if (binding.cheackposter.isChecked){
+                events.add(binding.cheackposter.text.toString())
+            }
+
+            if(events.size<=0){
+                Toast.makeText(requireContext(),"Chose Your Events",Toast.LENGTH_LONG).show()
+            }else{
+                if (events.size > 3){
+                    Toast.makeText(requireContext(),"You can Participate Maximum 3",Toast.LENGTH_LONG).show()
+                }else if(events.size == 1){
+                    Toast.makeText(requireContext(),"$events[0]",Toast.LENGTH_LONG).show()
+                }else if(events.size == 2){
+                    Toast.makeText(requireContext(),"$events[0] ",Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(requireContext(),"$events[0]",Toast.LENGTH_LONG).show()
+                }
+            }
+        }
         return root
     }
 
